6b207a709dafb28ea091330a489b1e84c42c8b6b
Daniel <daniel.c@itcgroup.io>
2023-12-19 07:23:24 +0700

    setup css module using scss

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import styles from "./page.module.scss"
 export default function Home() {
   return (
     <main className={styles.main}>
-
+        <h1>hello everyone</h1>
     </main>
   )
 }
